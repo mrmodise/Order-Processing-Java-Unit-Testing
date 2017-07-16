85b167b7199a309c5505abc48320eaf9cdbe4bc7
mrmodise <modisemorebodi@gmail.com>
2017-07-16 18:31:36 +0200

    added more tests on the order process

--- src/test/java/com/mrmodise/test/impl/OrderBOImplTest.java
+++ src/test/java/com/mrmodise/test/impl/OrderBOImplTest.java
@@ -11,6 +11,7 @@ import org.mockito.MockitoAnnotations;
 
 import java.sql.SQLException;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -22,25 +23,81 @@ public class OrderBOImplTest {
 
     @Mock
     OrderDAO orderDAO;
+    OrderBOImpl orderBO;
+    Order order;
 
     @Before
     public void setup(){
         MockitoAnnotations.initMocks(this);
+        orderBO = new OrderBOImpl();
+        orderBO.setOrderDAO(orderDAO);
+        order = new Order();
     }
 
     @Test
     public void placeOrder_Should_Create_An_Order() throws SQLException, BOException {
-        OrderBOImpl orderBO = new OrderBOImpl();
-        orderBO.setOrderDAO(orderDAO);
-
-        Order order = new Order();
         // setup expectation
         when(orderDAO.createOrder(order)).thenReturn(new Integer(1));
-
         boolean result = orderBO.placeOrder(order);
-
         // verify the results
         assertTrue(result);
         verify(orderDAO).createOrder(order);
     }
+
+    @Test
+    public void placeOrder_Should_Not_Create_An_Order() throws SQLException, BOException {
+        // setup expectation
+        when(orderDAO.createOrder(order)).thenReturn(new Integer(0));
+        boolean result = orderBO.placeOrder(order);
+        // verify the results
+        assertFalse(result);
+        verify(orderDAO).createOrder(order);
+    }
+
+    @Test(expected = BOException.class)
+    public void placeOrder_Should_Throw_BOException() throws SQLException, BOException {
+        // setup expectation to throw BO exception
+        when(orderDAO.createOrder(order)).thenThrow(SQLException.class);
+        // place an order
+        orderBO.placeOrder(order);
+    }
+
+    @Test
+    public void cancelOrder_Should_Cancel_Order() throws SQLException, BOException {
+        // setup expectation to throw BO exception
+        when(orderDAO.readOrder(123)).thenReturn(order);
+        when(orderDAO.updateOrder(order)).thenReturn(1);
+        boolean result = orderBO.cancelOrder(123);
+        assertTrue(result);
+        verify(orderDAO).readOrder(123);
+        verify(orderDAO).updateOrder(order);
+    }
+
+    @Test
+    public void cancelOrder_Should_Not_Cancel_Order() throws SQLException, BOException {
+        // setup expectation to throw BO exception
+        when(orderDAO.readOrder(123)).thenReturn(order);
+        when(orderDAO.updateOrder(order)).thenReturn(0);
+        boolean result = orderBO.cancelOrder(123);
+        assertFalse(result);
+        verify(orderDAO).readOrder(123);
+        verify(orderDAO).updateOrder(order);
+    }
+
+    @Test(expected = BOException.class)
+    public void cancelOrder_Should_Throw_SQL_Exception_On_Read() throws SQLException, BOException {
+        // setup expectation to throw BO exception
+        when(orderDAO.readOrder(123)).thenThrow(SQLException.class);
+        orderBO.cancelOrder(123);
+    }
+
+    @Test(expected = BOException.class)
+    public void cancelOrder_Should_Throw_SQLException_On_Update() throws SQLException, BOException {
+        // setup expectation to throw BO exception
+        when(orderDAO.readOrder(123)).thenReturn(order);
+        when(orderDAO.updateOrder(order)).thenThrow(SQLException.class);
+        orderBO.cancelOrder(123);
+    }
+
+
 }
